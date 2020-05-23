5babef2ca6f1c7ee81c7ece05e635ba3317a6f09
IshaShalvi <ishashalvi98@gmail.com>
2020-05-23 15:53:56 +0530

    creating forms for sign in and ssign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,27 @@
 module.exports.profile=function(req,res){
-    res.end('<h1>User Profile!!</h1>');
+    res.render('user_profile',{
+        title:"User Profile"
+    });
 
+}
+
+module.exports.signUp=function(req,res){
+   return res.render('user_sign_up',{
+       title:"Socio  | Sign Up"
+   });
+}
+
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Socio | Sign In"
+    });
+}
+
+module.exports.create=function(req,res){
+    //todo later
+}
+
+module.exports.createSession=function(req,res){
+    //todo later
 }
