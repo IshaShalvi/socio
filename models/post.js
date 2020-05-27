c20d59866b55b5350e693737eed74e3d383c087b
IshaShalvi <ishashalvi98@gmail.com>
2020-05-27 22:03:16 +0530

    adding comments to db

--- models/post.js
+++ models/post.js
@@ -10,10 +10,12 @@ const postSchema=new mongoose.Schema({
         type:mongoose.Schema.Types.ObjectId,
         ref:'User'
     },
-    comments:[{
+    comments:[
+        {
         type:mongoose.Schema.Types.ObjectId,
-        ref='Comment'
-    }]
+        ref:'comment'
+         }
+        ]
 },{
     timestamps:true
 });
