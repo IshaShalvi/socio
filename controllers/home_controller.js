4227dd78d72b9b77a5cbcfd19a1662755787414b
IshaShalvi <ishashalvi98@gmail.com>
2020-05-29 21:22:47 +0530

    creating flash messages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 const Post=require('../models/post');
 const User=require('../models/user');
 
-module.exports.home=function(req,res){
+module.exports.home=async function(req,res){
     // Post.find({},function(err,posts){
     //     return res.render('Socio | home',{
     //         title:"Home",
@@ -9,23 +9,32 @@ module.exports.home=function(req,res){
 
     // });
     // });
-    Post.find({})
+    try{
+
+        let posts=await Post.find({})
     .populate('user')
     .populate({
         path:'comments',
         populate:{
             path:'user'
         }
-    })
-    
-    .exec(function(err,posts){
-        User.find({},function(err,users){
-            return res.render('home',{
-                title:"Socio | home",
-                posts:posts,
-                all_users:users
-
-             });
-        });
     });
+    
+   
+    let users=await User.find({});
+
+    return res.render('home',{
+        title:"Socio | home",
+        posts:posts,
+        all_users:users
+
+    });
+        
+
+    }catch(err)
+    {
+        console.log('Error',err);
+        return;
+    }
+    
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -55,12 +55,15 @@ module.exports.create=function(req,res){
 }
 
 module.exports.createSession=function(req,res){
+    req.flash('success','Logged in successfully!');
     return res.redirect('/');
+
    
 }
 
 module.exports.destroySession=function(req,res){
     req.logout();
+    req.flash('success','Logged out successfully!');
 
     return res.redirect('/');
 
